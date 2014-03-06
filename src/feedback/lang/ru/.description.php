4fb9e6b27ef2abeefbfec5a37845c6e5ebcfc5a7
onEXHovia <onexhovia@gmail.com>
2014-03-06 15:14:42 +0600

    Переместил файлы компонента

---
+++ src/feedback/lang/ru/.description.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+//$MESS[''] = '';
