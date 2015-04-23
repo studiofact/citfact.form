619a55538828e23d36201a3d9c5d16ef6b0c5979
onexhovia <onexhovia@gmail.com>
2015-04-23 16:12:21 +0500

    Fix CS

--- lib/Extension/IdentifierExtension.php
+++ lib/Extension/IdentifierExtension.php
@@ -1,59 +1,58 @@
 <?php
 
-    /*
-	 * This file is part of the Studio Fact package.
-	 *
-	 * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
-	 *
-	 * For the full copyright and license information, please view the LICENSE
-	 * file that was distributed with this source code.
-	 */
+/*
+ * This file is part of the Studio Fact package.
+ *
+ * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-    namespace Citfact\Form\Extension;
+namespace Citfact\Form\Extension;
 
-    class IdentifierExtension
+class IdentifierExtension
+{
+    /**
+     * Generates a identifier
+     *
+     * @return string
+     */
+    public function generateIdentifier()
     {
-        /**
-         * Generates a identifier
-         *
-         * @return string
-         */
-        public function generateIdentifier()
-        {
-            return $this->getIdentifier();
-        }
-
-        /**
-         * Validates a identifier token
-         *
-         * @param string $identifier
-         * @return bool
-         */
-        public function  isIdentifierValid($identifier)
-        {
-            return $identifier === $this->generateIdentifier();
-        }
-
-        /**
-         * Return identifier token
-         *
-         * @return string
-         */
-        protected function getIdentifier()
-        {
-            $caller = defined('DEBUG_BACKTRACE_IGNORE_ARGS') ? debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS) : debug_backtrace(false);
-
-            $trace_count = count($caller);
-            $trace_current = $trace_count-1;
-
-            for ($i = 0; $i<$trace_count; $i++)
-            {
-                if (strtolower($caller[$i]['function']) == 'includecomponent' && (($c = strtolower($caller[$i]['class'])) == 'callmain' || $c == 'cmain'))
-                {
-                    $trace_current = $i;
-                    break;
-                }
-            }
-            return (string)crc32(sprintf('%s_%s', $caller[$trace_current]['file'], $caller[$trace_current]['line']));
-        }
+        return $this->getIdentifier();
     }
+
+    /**
+     * Validates a identifier token
+     *
+     * @param string $identifier
+     * @return bool
+     */
+    public function  isIdentifierValid($identifier)
+    {
+        return $identifier === $this->generateIdentifier();
+    }
+
+    /**
+     * Return identifier token
+     *
+     * @return string
+     */
+    protected function getIdentifier()
+    {
+        $caller = defined('DEBUG_BACKTRACE_IGNORE_ARGS') ? debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS) : debug_backtrace(false);
+
+        $trace_count = count($caller);
+        $trace_current = $trace_count - 1;
+
+        for ($i = 0; $i < $trace_count; $i++) {
+            if (strtolower($caller[$i]['function']) == 'includecomponent' && (($c = strtolower($caller[$i]['class'])) == 'callmain' || $c == 'cmain')) {
+                $trace_current = $i;
+                break;
+            }
+        }
+        
+        return (string)crc32(sprintf('%s_%s', $caller[$trace_current]['file'], $caller[$trace_current]['line']));
+    }
+}
