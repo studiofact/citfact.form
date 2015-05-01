14f0f0565f1e33a426f085150ed7df82d78492f4
Denis Kulichkin <onexhovia@gmail.com>
2015-05-01 09:22:25 +0300

    Fix CS

--- install/components/citfact/form/templates/.default/script.js
+++ install/components/citfact/form/templates/.default/script.js
@@ -1,64 +1,66 @@
-'use strict';
+(function (window, $) {
+  'use strict';
 
-/**
- * Construct object
- *
- * @param object params
- * @constructor
- */
-var FormGenerator = function (params) {
+  /**
+   * Construct object
+   *
+   * @param object params
+   * @constructor
+   */
+  var FormGenerator = function (params) {
     this.form = params.formContainer;
     this.ajaxMode = params.ajaxMode;
     this.captchaImg = params.captchaImg;
     this.captchaReload = params.captchaReload;
     this.uri = params.uri;
-}
+  }
 
-/**
- * Init events
- */
-FormGenerator.prototype.init = function () {
+  /**
+   * Init events
+   */
+  FormGenerator.prototype.init = function () {
     this.reloadCaptcha();
     if (this.ajaxMode) {
-        this.submitForm();
+      this.submitForm();
     }
-}
+  }
 
-/**
- * Event submit form when ajaxMode = true
- */
-FormGenerator.prototype.submitForm = function () {
+  /**
+   * Event submit form when ajaxMode = true
+   */
+  FormGenerator.prototype.submitForm = function () {
     var self = this;
     $(document).on('submit', this.form, function () {
-        $.post(self.uri, $(self.form).serialize(), function (response) {
-            $(self.form).replaceWith(response.html);
-            self.reloadCaptcha();
-        });
+      $.post(self.uri, $(self.form).serialize(), function (response) {
+        $(self.form).replaceWith(response.html);
+        self.reloadCaptcha();
+      });
 
-        return false;
+      return false;
     });
-}
+  }
 
-/**
- * Event reload captcha
- */
-FormGenerator.prototype.reloadCaptcha = function () {
+  /**
+   * Event reload captcha
+   */
+  FormGenerator.prototype.reloadCaptcha = function () {
     var self = this;
     $(this.form).on('click', this.captchaReload, function () {
-        $.post(self.uri, $(self.form).serialize(), function (response) {
-            self.setCaptcha(response.captcha);
-        });
+      $.post(self.uri, $(self.form).serialize(), function (response) {
+        self.setCaptcha(response.captcha);
+      });
 
-        return false;
+      return false;
     });
-}
+  }
 
-/**
- * Set new captcha
- *
- * @param string code
- */
-FormGenerator.prototype.setCaptcha = function (code) {
+  /**
+   * Set new captcha
+   *
+   * @param string code
+   */
+  FormGenerator.prototype.setCaptcha = function (code) {
     $(this.form).find('input[name*=captcha_sid]').val(code);
     $(this.form).find(this.captchaImg).prop('src', '/bitrix/tools/captcha.php?captcha_sid=' + code);
-}
+  }
+})(window, window.jQuery);
