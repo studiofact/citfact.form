e5a746f2ddc3f866d7828f9f05c2e56a85d4f0f6
kid_clique <my.box686@gmail.com>
2015-07-13 15:18:12 +0300

    Composit

--- install/components/citfact/form.view/templates/date/template.php
+++ install/components/citfact/form.view/templates/date/template.php
@@ -4,6 +4,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
+$this->setFrameMode(true);
 CJSCore::Init(array('date'));
 ?>
 
--- install/components/citfact/form.view/templates/file/template.php
+++ install/components/citfact/form.view/templates/file/template.php
@@ -3,6 +3,8 @@
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 }
+
+$this->setFrameMode(true);
 ?>
 
 <div class="form-group">
