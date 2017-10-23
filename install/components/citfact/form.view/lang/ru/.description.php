e79c079dda15f464fd295f78a7d6ca147d2de05f
Dmitriy Larionov <dml@crtweb.ru>
2017-10-23 17:02:05 +0500

    add description for form.view component

---
+++ install/components/citfact/form.view/lang/ru/.description.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the Studio Fact package.
+ *
+ * (c) Kulichkin Denis (onEXHovia) <onexhovia@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$MESS['NAME'] = 'Поля генератора форм';
+$MESS['DESCRIPTION'] = 'Отображает поля Генератора форм';
+$MESS['CITFACT_NAME'] = 'ЦИТ "ФАКТ"';
